f7baa1d57713a013c81111b289239f4a3b64a68b
OllyButters <oliver.butters@gmail.com>
2019-05-30 21:40:10 +0100

    restructuring format and script flow. CURRENTLY BROKEN.

--- source/status.py
+++ source/status.py
@@ -16,29 +16,43 @@
 
 
 import glob
+import pprint
+import re
 import os.path
 import xml.etree.ElementTree as ET
 
 __author__ = "Olly Butters"
 __date__ = 28/3/19
 
-local_root_path = "./"
+# local_root_path = "./"
+local_root_path = "/home/olly/git/"
 remote_root_path = "http://github.com/datashield/"
 repo_name = "dsBetaTestClient"
 branch_name = "master"
 output_file_name = "status.html"
-# devtools_test_output_file = "logs/Job/6_Devtools tests.txt"
-devtools_test_output_file = "test_results.xml"
+devtools_test_output_file = "../logs/test_results.xml"
+# devtools_test_output_file = "test_results.xml"
+
+
+pp = pprint.PrettyPrinter(indent=4)
 
 local_repo_path = local_root_path + repo_name
 remote_repo_path = remote_root_path + repo_name
 
-
 # Check repo exists
+print("local repo path: " + local_repo_path)
+print("remote repo path: " + remote_repo_path)
+
+
+
 
 ################################################################################
 # Get list of functions from R folder
+#
+print("\n\n##########")
 ds_functions_path = glob.glob(local_repo_path + "/R/*.R")
+
+print("Number of local functions found: " + str(len(ds_functions_path)))
 
 ds_functions = []
 for this_path in ds_functions_path:
@@ -51,9 +65,19 @@ for this_function in ds_functions:
     print(this_function)
 
 
+# Make the test status dictionary
+ds_test_status = {}
+for this_function in ds_functions:
+    ds_test_status[this_function] = {}
+
+
+
 ################################################################################
 # Get the list of tests
+print("\n\n##########")
 ds_tests_path = glob.glob(local_repo_path + "/tests/testthat/*.R")
+
+print("Number of local test files found: " + str(len(ds_tests_path)))
 
 ds_tests = []
 for this_test in ds_tests_path:
@@ -65,13 +89,20 @@ ds_tests.remove('teardown.R')
 
 ds_tests.sort()
 
+for this_test in ds_tests:
+    print(this_test)
+
+# See if test files exist for all the functions
+
+
 ################################################################################
 # Parse the devtools::tests() log file, this is the output of the testthat tests
 #devtools_h = open(devtools_test_output_file, "r")
 #devtools_results = devtools_h.read()
 #print(devtools_results)
+print("\n\n##########")
 
-print("XML!!!")
+print("Parsing XML!!!")
 
 tree = ET.parse(devtools_test_output_file)
 root= tree.getroot()
@@ -79,37 +110,63 @@ root= tree.getroot()
 print(root.tag)
 
 # Define status dictionary and assign zeros for each test (NOTE: this is like ds.cov.R)
-ds_test_status = {}
-for this_test in ds_tests:
-    ds_test_status[this_test] = {'number':0, 'errors':0}
+# for this_test in ds_tests:
+#     ds_test_status[this_test] = {'number':0, 'errors':0}
 
 # Cycle through the xml line by line. This will have data for ALL tests.
 for child in root:
     print(child.attrib['name'], child.attrib['tests'], child.attrib['errors'])
 
-    # Look to see if this test has a match in the existing list of tests
-    for this_test in ds_tests:
-        #print(this_test[5:-2])
-        # Dont need the 'test-' and the '.R' bit of the test name
-        if this_test[5:-2] in child.attrib['name']:
-            ds_test_status[this_test]['number'] = int(ds_test_status[this_test]['number']) +int(child.attrib['tests'])
-            ds_test_status[this_test]['errors'] = int(ds_test_status[this_test]['errors']) +int(child.attrib['errors'])
+    temp_name = child.attrib['name']
+    temp_name = temp_name[18:]   # Drop the dsBetaTestClient:: prefix
+    re.sub("([\(\[]).*?([\)\]])", "\g<1>\g<2>", temp_name)   # Drop brackets and contents
 
-print(ds_test_status)
+    print(temp_name)
+
+    # Look to see if this test has a match in the existing list of tests
+    #for this_test in ds_tests:
+        # Dont need the 'test-' and the '.R' bit of the test name
+    #    if this_test[5:-2] in child.attrib['name']:
+    #        ds_test_status[this_test]['number'] = int(ds_test_status[this_test]['number']) +int(child.attrib['tests'])
+    #        ds_test_status[this_test]['errors'] = int(ds_test_status[this_test]['errors']) +int(child.attrib['errors'])
+
+    try:
+        ds_test_status[this_test]['number'] = int(ds_test_status[this_test]['number']) +int(child.attrib['tests'])
+        ds_test_status[this_test]['errors'] = int(ds_test_status[this_test]['errors']) +int(child.attrib['errors'])
+    except:
+        pass
+
+pp.pprint(ds_test_status)
 
 
 ################################################################################
 #Make some html
+#
+print("\n\n##########")
+
 h = open(output_file_name, "w")
 h.write('<!DOCTYPE html>\n<html>\n<head>\n<link rel="stylesheet" href="status.css">\n</head>\n<body>')
 
 h.write("<h2>" + repo_name + "</h2>")
 
 h.write("<table border=1>")
-h.write("<tr><th>Function name</th><th>Test file exist</th><th>Number of tests</th><th>Test pass</th></tr>")
+h.write("<tr><th>Function name</th><th>Smoke test file exist</th><th>Test file exist</th><th>Number of tests</th><th>Test pass</th></tr>")
 for this_function in ds_functions:
     h.write("<tr>")
     h.write("<td><a href=" + remote_repo_path + "/blob/" + branch_name + "/R/" + this_function + ">" + this_function + "</a></td>")
+
+    ####################
+    # Smoke test
+    # See if test exists
+    expected_test_name = "test-smk-"+this_function
+    print(expected_test_name)
+    if expected_test_name in ds_tests:
+        h.write('<td class="good"><a href="' + remote_repo_path + '/blob/' + branch_name + '/tests/testthat/' + expected_test_name + '">' + expected_test_name + '</a></td>')
+    else:
+        h.write("<td></td>")
+
+    # dsBetaTestClient::ds.cor.o():smoke
+
 
     # See if test exists
     expected_test_name = "test-"+this_function
